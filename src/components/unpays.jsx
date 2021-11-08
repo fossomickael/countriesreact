a412ba102deda6dc8043d898c07b0c96135f6024
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-08 11:08:52 +0100

    add responsivness to one country display

--- src/components/unpays.jsx
+++ src/components/unpays.jsx
@@ -42,30 +42,31 @@ class UnPays extends Component {
           }
         return (
             <div>
-        <div><Back /></div><div className="unpays">
+        <div><Back /></div>
+        <div className="unpays">
         
-        <div>
-            <img className="unpaysimg" alt="drapeau" src={`${this.props.unpays.flag}`} />
-        </div>
-        <div className="paysdroite">
-            <h2>{`${this.props.unpays.name}`}</h2>
-            <div className="unpaysdetails">   
-            <ul>
+            <div>
+                <img className="unpaysimg" alt="drapeau" src={`${this.props.unpays.flag}`} />
+            </div>
+            <div className="paysdroite">
+                <h2>{`${this.props.unpays.name}`}</h2>
+                <div className="unpaysdetails">   
+                <ul>
+                    
+                    <li><b>Native Name:</b> {`${this.props.unpays.nativeName}`}</li>
+                    <li><b>Population:</b> {`${this.props.unpays.population}`}</li>
+                    <li><b>Region:</b> {`${this.props.unpays.region}`}</li>
+                    <li><b>Capital:</b> {`${this.props.unpays.capital}`}</li>
+                </ul>
+                <ul className="otherdetails">
+                    <li><b>Top level domain:</b> {`${this.props.unpays.topLevelDomain}`}</li>
+                    <li><b>Currencies:</b> {`${this.extractvalues(this.props.unpays.currencies, "name")}`}</li>
+                    <li><b>Languages:</b> {`${this.extractvalues(this.props.unpays.languages, "name")}`}</li>
+                </ul>
                 
-                <li><b>Native Name:</b> {`${this.props.unpays.nativeName}`}</li>
-                <li><b>Population:</b> {`${this.props.unpays.population}`}</li>
-                <li><b>Region:</b> {`${this.props.unpays.region}`}</li>
-                <li><b>Capital:</b> {`${this.props.unpays.capital}`}</li>
-            </ul>
-            <ul>
-                <li><b>Top level domain:</b> {`${this.props.unpays.topLevelDomain}`}</li>
-                <li><b>Currencies:</b> {`${this.extractvalues(this.props.unpays.currencies, "name")}`}</li>
-                <li><b>Languages:</b> {`${this.extractvalues(this.props.unpays.languages, "name")}`}</li>
-             </ul>
-            
-             </div>
-              <div className="border"><b>Border Countries:</b> {`${this.extractborders(this.props.unpays.borders)}`}</div>
-   
+                </div>
+                <div className="border"><b>Border Countries:</b> {`${this.extractborders(this.props.unpays.borders)}`}</div>
+    
             </div>    
 
 
