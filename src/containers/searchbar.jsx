09ab2ae76cc7105eb824358c97c5e3861e5b1109
Mickael FOSSO <fosso.mickael@gmail.com>
2021-12-01 20:10:28 +0100

    fix error that added countries twice to state

--- src/containers/searchbar.jsx
+++ src/containers/searchbar.jsx
@@ -1,51 +1,42 @@
-import React, { Component } from 'react'; 
-import { bindActionCreators } from 'redux';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 import { cherchePays, setPays } from "../actions/pays";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class SearchBar extends Component {
-
-    handleUpdate = (event) => {
-        if (event.target.value === '') {
-            this.props.setPays();
-        }
-        else
-        {
-           this.props.cherchePays(event.target.value);
-        }
-        
+  handleUpdate = (event) => {
+    if (event.target.value === "") {
+      this.props.setPays();
+    } else {
+      this.props.cherchePays(event.target.value);
     }
+  };
 
-    render() {
-        return (
-          <div className="searchbar">
-           
-            <input
-              type="text"
-              className="form-control form-search"
-              onChange={this.handleUpdate}
-              placeholder="Search for a country"
-            />
-            <button type="submit" class="search-button">
-              <i className="fas fa-search"></i>
-            </button>
-             
-            </div>
-        );
-      }
-
+  render() {
+    return (
+      <div className="searchbar">
+        <input
+          type="text"
+          className="form-control form-search"
+          onChange={this.handleUpdate}
+          placeholder="Search for a country"
+        />
+        <button type="submit" className="search-button">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      pays: state.pays
-    };
+  return {
+    pays: state.pays,
   };
-  
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({ cherchePays, setPays }, dispatch);
 };
 
-
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ cherchePays, setPays }, dispatch);
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
